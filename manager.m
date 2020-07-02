e43e0dc8d66c176627118051e10369e3335cea06
barbar98 <bu3@op.pl>
2020-07-02 17:31:00 +0200

    manager upgrade, procenty
    wylicza procent dobrze rozpoznanych dojrzałych i zielonych. zasadniczo nie działa :p

--- manager.m
+++ manager.m
@@ -12,6 +12,8 @@ n_good = length(good_pict_list);
 n_rotten = length(rotten_pict_list);
 n_green = length(green_pict_list);
 
+positive_good = 0;
+positive_green = 0;
 
 addpath './P_2_wizja_Artur/HSV_seg'
 
@@ -19,12 +21,14 @@ for i = 1:size(good_strwber,1)
    current_filename = good_pict_list(randi(n_good,1));
    image = imread(strcat("P_2_wizja_Artur/HSV_seg/good_std/",current_filename.name));
    [good_present, green_present] = vision_algo(image);
+   if (good_present == 1) positive_good = positive_good + 1; end
 end
 
 for i = 1:size(green_strwber,1)
    current_filename = green_pict_list(randi(n_green),1);
    image = imread(strcat("P_2_wizja_Artur/HSV_seg/green_std/",current_filename.name));
    [good_present, green_present] = vision_algo(image);
+   if (green_present == 1) positive_green = positive_green + 1; end
 end
 
 for i = 1:size(rotten_strwber,1)
@@ -33,3 +37,5 @@ for i = 1:size(rotten_strwber,1)
    [good_present, green_present] = vision_algo(image);
 end
 
+percent_pos_good = positive_good/size(good_strwber,1)
+percent_pos_green = positive_green/size(green_strwber,1)
